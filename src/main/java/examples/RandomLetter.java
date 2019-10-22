3c332dfcecbacd5a2ecdb588c099e3081828d568
Zem <karimovazemfira@yahoo.com>
2019-10-22 10:43:05 +0400

    -- changed

--- src/main/java/examples/RandomLetter.java
+++ src/main/java/examples/RandomLetter.java
@@ -1,15 +1,30 @@
 package examples;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomLetter {
     public static String gen_rand(int length){
         Random r = new Random();
-        String l = String.valueOf(r.nextInt(26) + 'a');
+        String l = "";
+
+        for (int i=0; i<length; i++) {
+            int number = (char) (r.nextInt(26) + 'a');
+            int number1 = (char) (r.nextInt(26) + 'A');
+
+            char c = (char) number;
+            char c1 = (char) number1;
+
+            l = l + c+ c1;
+        }
+
         return l;
     }
     public static void main(String[] args) {
-        String s = gen_rand(5);
-        System.out.println(s);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length: ");
+        int n = sc.nextInt();
+        String s = gen_rand(n);
+        System.out.println(s.substring(0,n));
     }
 }
